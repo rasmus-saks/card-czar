39dcd03ab102d241c42fd6da3597bb0c03333b02
Rasmus Saks <bogeyman.est@gmail.com>
2016-03-06 01:51:04 +0200

    Add Elastic Beanstalk deploy task to Grunt and make JSHint scan client-side javascript files as well

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,17 +2,22 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js', 'public/js/**.js'],
       options: {
         globals: {
           jQuery: true
         }
       }
+    },
+    exec: {
+      deploy: "eb deploy"
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-exec');
 
   grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('deploy', ['jshint', 'exec']);
 
 };
