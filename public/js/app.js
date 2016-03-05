2204fe4620db0ec8e15f58696096af8e6f8277d6
Rezonation <TheAoihoshi@gmail.com>
2016-03-05 19:15:21 +0200

    Add front-end support for socket.io

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,18 @@
 (function () {
   var app = angular.module('CardCzar', ['CardCzar.filters', 'CardCzar.services']);
-  app.controller("CCMainCtrl", function AppCtrl($scope, $http) {
+  app.controller("CCMainCtrl", function AppCtrl($scope, $http, socket) {
     //operation initiated when controller is constructed
     var ccmain = this;
-    ccmain.hello = "Hello, World!"
+    ccmain.hello = "Hello,";
+    socket.on('init', function (data) {
+      ccmain.name = data.name;
+      ccmain.users = data.users;
+    });
+    ccmain.selectCard = function (cardid) {
+      socket.emit('send:cardSelection', {
+        selection: cardid
+      });
+    }
+
   });
 })();
--- public/js/services.js
+++ public/js/services.js
@@ -2,4 +2,27 @@
  * Created by AlWilliam on 3/5/2016.
  */
 angular.module('CardCzar.services', []).
-  value('version', '0.1');
+  value('version', '0.1')
+  .factory('socket', function ($rootScope) {
+    var socket = io.connect();
+    return {
+      on: function (eventName, callback) {
+        socket.on(eventName, function () {
+          var args = arguments;
+          $rootScope.$apply(function () {
+            callback.apply(socket, args);
+          });
+        });
+      },
+      emit: function (eventName, data, callback) {
+        socket.emit(eventName, data, function () {
+          var args = arguments;
+          $rootScope.$apply(function () {
+            if (callback) {
+              callback.apply(socket, args);
+            }
+          });
+        })
+      }
+    };
+  });
