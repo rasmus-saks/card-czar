50bc4837ecb48ddcf15728ac222760d6e5583d3c
Rasmus Saks <bogeyman.est@gmail.com>
2016-03-06 01:45:12 +0200

    Add JSHint Grunt task

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('default', ['jshint']);
+
+};
