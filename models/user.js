94aabdf4497b82fdf03450a50ab5d3f7eefce28e
Rasmus Saks <bogeyman.est@gmail.com>
2016-03-05 21:36:03 +0200

    Change logging format and connect to MySQL on startup

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
-  var user = sequelize.define('user', {
+  var User = sequelize.define('User', {
     name: DataTypes.STRING
   }, {
     classMethods: {
@@ -9,5 +9,5 @@ module.exports = function(sequelize, DataTypes) {
       }
     }
   });
-  return user;
+  return User;
 };
