d2a723a2bdb30d1d7e65642b8fad049372789473
Rasmus Saks <bogeyman.est@gmail.com>
2016-03-06 21:53:06 +0200

    Send test data on socket connection

--- src/app/socket.js
+++ src/app/socket.js
@@ -3,7 +3,11 @@ var socket_io = require('socket.io');
 function Socket(server) {
   var io = socket_io(server);
   io.on('connection', function (socket) {
-
+    socket.emit("init", {
+      name: "Name",
+      users: [{name: "User1"}, {name: "User2"}],
+      lobbycode: "123ABC"
+    })
   });
   return io;
 
