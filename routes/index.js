6be0e7bdc65a6b161d925587021292e686bc7a29
Rezonation <TheAoihoshi@gmail.com>
2016-03-13 16:51:43 +0200

    Add login path and fix landing styling

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+router.get("/login", function (req, res) {
+  res.render('login');
+});
+
 router.get("*", function (req, res) {
   res.render('landing');
 });
