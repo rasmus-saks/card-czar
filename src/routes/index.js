9c1ad0d63e009fe111119641f8f8bef82c4267cd
Rezonation <TheAoihoshi@gmail.com>
2016-03-13 19:11:00 +0200

    Move all interface language to localization file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
+var en = require('../locale/en.json');
 var router = express.Router();
 
 router.get("/login", function (req, res) {
-  res.render('login');
+  res.render('login', {lang:en});
 });
 
 router.get("*", function (req, res) {
-  res.render('landing');
+  res.render('landing', {lang:en});
 });
 
 module.exports = router;
