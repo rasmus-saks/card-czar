b994fb583f6db4033b2f88f0d4c610dc1c5d44f0
Rasmus Saks <bogeyman.est@gmail.com>
2016-03-21 00:46:02 +0200

    Fix landing page counts

--- public/js/landing.js
+++ public/js/landing.js
@@ -5,8 +5,8 @@ $('#lobbycode').on('input propertychange paste', function () {
   $('#joinlobby').attr("href", "/game/" + $('#lobbycode').val());
 });
 $.get("/api/users", function(data, status){
-  $('#users').text(data);
+  $('#users').text(data.data);
 });
 $.get("/api/totalgames", function(data, status){
-  $('#games').text(data);
+  $('#games').text(data.data);
 });
