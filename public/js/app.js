a991701b6f202950218fe04ef499656668e7de2e
Rezonation <TheAoihoshi@gmail.com>
2016-03-25 14:43:02 +0200

    Add partial game logic handling

--- public/js/app.js
+++ public/js/app.js
@@ -2,18 +2,31 @@
   var app = angular.module('CardCzar', ['CardCzar.filters', 'CardCzar.services']);
   app.controller("CCMainCtrl", function AppCtrl($scope, $http, socket) {
     //operation initiated when controller is constructed
+    socket.emit('join', lobbycode);
     var ccmain = this;
+
     ccmain.hello = "Hello,";
     socket.on('init', function (data) {
-      ccmain.name = data.name;
+      ccmain.player = data.player;
       ccmain.users = data.users;
       ccmain.lobbycode = data.lobbycode;
+      ccmain.game = data.game;
     });
+    socket.on('newround', function (data) {
+      ccmain.player = data.player;
+      ccmain.users = data.users;
+      ccmain.game = data.game;
+    });
+    socket.on('updateplayers', function (data) {
+      ccmain.player = data.player;
+    });
+
     ccmain.selectCard = function (cardid) {
       socket.emit('send:cardSelection', {
         selection: cardid
       });
     };
+
   });
   app.controller("CCDeckBrowser", function($scope, $http, Deck, Card) {
     this.decks = Deck.query(function() {
